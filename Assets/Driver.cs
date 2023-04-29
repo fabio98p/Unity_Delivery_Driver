5c4af483a089813cf5ebc5b13983877b229b4972
fabio98p <parisifabio98@gmail.com>
2023-04-29 19:01:28 +0200

    basic movment of car

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float moveSpeed = 20f;
+
     void Start()
     {
-        //transform.Rotate(0, 0, 45);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0.0f, moveAmount, 0.0f);
     }
 }
