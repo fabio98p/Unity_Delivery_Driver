530a981a247613c15e6216639a3ff6d7e3b429a4
fabio98p <parisifabio98@gmail.com>
2023-04-30 14:35:55 +0200

    end delivery

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(0, 255, 0, 255); 
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
+    SpriteRenderer spriteRenderer;
     private bool havePackage = false;
     [SerializeField] float timeToDestroy = 0f;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateCarColor();
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Ouch");
@@ -19,11 +30,24 @@ public class Delivery : MonoBehaviour
             Debug.Log(collision.tag);
             havePackage = true;
             Destroy(collision.gameObject, timeToDestroy);
+            UpdateCarColor();
         }
         if (collision.tag == "Customer" && havePackage)
         {
             Debug.Log(collision.tag);
             havePackage = false;
+            UpdateCarColor();
+        }
+    }
+    private void UpdateCarColor()
+    {
+        if (havePackage)
+        {
+            spriteRenderer.color = hasPackageColor;
+        }
+        else
+        {
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
