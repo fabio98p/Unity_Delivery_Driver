7deaa1d3ea33381d8c6e28cf417b936cfccc479b
fabio98p <parisifabio98@gmail.com>
2023-04-30 15:02:13 +0200

    create boost trigger

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float slowSpeed = 10f;
     [SerializeField] float moveSpeed = 20f;
-
-    void Start()
-    {
-
-    }
+    [SerializeField] float boostSpeed = 50f;
 
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float moveAmount;
+        moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0.0f, moveAmount, 0.0f);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
